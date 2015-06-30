8ff22ef869393a405a8443aabcbd24633c3ae959
jerpeter <jerpeter@yahoo.com>
2015-06-30 15:54:58 -0500

    Release 2.18.F

--- src/Version.c
+++ src/Version.c
@@ -8,7 +8,7 @@
 //=====================================================
 // Build version, system date and time referenced
 //=====================================================
-const char g_buildVersion[] = "2.18.E";
+const char g_buildVersion[] = "2.18.F";
 const char g_buildDate[] = __DATE__ " " __TIME__;
 // =====================================================
 
