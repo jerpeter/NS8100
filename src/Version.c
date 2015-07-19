77d51453f080df17200f23e637b305c0b508a243
jerpeter <jerpeter@yahoo.com>
2015-07-19 16:57:17 -0500

    Release 2.19.C

--- src/Version.c
+++ src/Version.c
@@ -8,7 +8,7 @@
 //=====================================================
 // Build version, system date and time referenced
 //=====================================================
-const char g_buildVersion[] = "2.19.B";
+const char g_buildVersion[] = "2.19.C";
 const char g_buildDate[] = __DATE__ " " __TIME__;
 // =====================================================
 
