b4c58d7b881bef583d354da92d197ed5708afbd5
jerpeter <jerpeter@yahoo.com>
2015-11-06 01:25:40 -0600

    Removed some unnecessary pointers to bargraph globals Updated references to now use bargraph structures

--- src/InitDataBuffers.c
+++ src/InitDataBuffers.c
@@ -46,6 +46,7 @@
 void InitDataBuffs(uint8 opMode)
 { 
 	uint32 pretriggerBufferSize;
+	uint32 barIntervalOffsetInWords;
 	uint32 sampleRate;
 	
 	if (opMode == MANUAL_CAL_MODE)
@@ -85,7 +86,9 @@ void InitDataBuffs(uint8 opMode)
 	InitEventRecord(opMode);
 
 	// Setup buffers based on mode
+	//=============================================================================================
 	if ((opMode == WAVEFORM_MODE) || (opMode == MANUAL_CAL_MODE) || (opMode == COMBO_MODE))
+	//---------------------------------------------------------------------------------------------
 	{
 		// Calculate samples for each section and total event
 		g_samplesInPretrig = (uint32)(sampleRate / g_unitConfig.pretrigBufferDivider);
@@ -132,39 +135,42 @@ void InitDataBuffs(uint8 opMode)
 		g_doneTakingEvents = NO;
 		
 		// Update the data length to be used based on the size calculations
-		if (opMode == WAVEFORM_MODE)
-		{
-			// Update the waveform data length to be used based on the size calculations
-			g_pendingEventRecord.header.dataLength = (g_wordSizeInEvent * 2);
-		}
-		else if (opMode == MANUAL_CAL_MODE)
+		if (opMode == MANUAL_CAL_MODE)
 		{
 			// Update the manual cal data length to be used based on the size calculations
 			g_pendingEventRecord.header.dataLength = (g_wordSizeInCal * 2);
 		}
-		else if (opMode == COMBO_MODE)
-		{		
-			// Update the combo-waveform data length to be used based on the size calculations
+		else // ((opMode == WAVEFORM_MODE) || (opMode == COMBO_MODE))
+		{
+			// Update the waveform data length to be used based on the size calculations
 			g_pendingEventRecord.header.dataLength = (g_wordSizeInEvent * 2);
-
-			// Bargraph init
-			g_bargraphDataStartPtr = &(g_eventDataBuffer[0]);
-			g_bargraphDataWritePtr = &(g_eventDataBuffer[0]);
-			g_bargraphDataReadPtr = &(g_eventDataBuffer[0]);
-			g_bargraphDataEndPtr = &(g_eventDataBuffer[COMBO_MODE_BARGRAPH_BUFFER_SIZE_WORDS]);
-
-			// Start the total off with zero (incremented when bar and summary intervals are stored)
-			g_pendingBargraphRecord.header.dataLength = 0;
-
-			StartNewBargraph();
 		}
 	}
-	else if (opMode == BARGRAPH_MODE)
-	{		
-		g_bargraphDataStartPtr = &(g_eventDataBuffer[0]);
-		g_bargraphDataWritePtr = &(g_eventDataBuffer[0]);
-		g_bargraphDataReadPtr = &(g_eventDataBuffer[0]);
-		g_bargraphDataEndPtr = &(g_eventDataBuffer[EVENT_BUFF_SIZE_IN_WORDS]);
+
+	//=============================================================================================
+	if ((opMode == BARGRAPH_MODE) || (opMode == COMBO_MODE))
+	//---------------------------------------------------------------------------------------------
+	{
+		// Get the total size (in words) needed for caching the total number of Bar Intervals for the Summary Interval period selected (+1 spare)
+		barIntervalOffsetInWords = ((((g_triggerRecord.bgrec.summaryInterval / g_triggerRecord.bgrec.barInterval) + 1) * sizeof(BARGRAPH_BAR_INTERVAL_DATA)) / 2);
+
+		// Init Bar Interval pointers
+		g_bargraphBarIntervalWritePtr = (BARGRAPH_BAR_INTERVAL_DATA*)&(g_eventDataBuffer[0]);
+		g_bargraphBarIntervalReadPtr = (BARGRAPH_BAR_INTERVAL_DATA*)&(g_eventDataBuffer[0]);
+		g_bargraphBarIntervalEndPtr = (BARGRAPH_BAR_INTERVAL_DATA*)&(g_eventDataBuffer[barIntervalOffsetInWords]);
+
+		g_bargraphDataStartPtr = &(g_eventDataBuffer[barIntervalOffsetInWords]);
+		g_bargraphDataWritePtr = &(g_eventDataBuffer[barIntervalOffsetInWords]);
+		g_bargraphDataReadPtr = &(g_eventDataBuffer[barIntervalOffsetInWords]);
+
+		if (opMode == COMBO_MODE)
+		{
+			g_bargraphDataEndPtr = &(g_eventDataBuffer[COMBO_MODE_BARGRAPH_BUFFER_SIZE_WORDS]);
+		}
+		else // (opMode == BARGRAPH_MODE)
+		{
+			g_bargraphDataEndPtr = &(g_eventDataBuffer[EVENT_BUFF_SIZE_IN_WORDS]);
+		}
 
 		// Start the total off with zero (incremented when bar and summary intervals are stored)
 		g_pendingBargraphRecord.header.dataLength = 0;
