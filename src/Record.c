5aa2ac1850813b601f94b7ff67c3157ee2e91f28
jerpeter <jerpeter@yahoo.com>
2016-05-16 20:14:15 -0500

    Change trigger record default to 12 bit per Nomis request

--- src/Record.c
+++ src/Record.c
@@ -400,7 +400,7 @@ void LoadTrigRecordDefaults(REC_EVENT_MN_STRUCT* triggerRecordPtr, uint8 opMode)
 	triggerRecordPtr->trec.record_time = 3;
 	triggerRecordPtr->trec.airTriggerLevel = NO_TRIGGER_CHAR;
 	triggerRecordPtr->trec.adjustForTempDrift = YES;
-	triggerRecordPtr->trec.bitAccuracy = ACCURACY_16_BIT;
+	triggerRecordPtr->trec.bitAccuracy = ACCURACY_12_BIT;
 	triggerRecordPtr->bgrec.barInterval = SIXTY_SEC_PRD;
 	triggerRecordPtr->bgrec.summaryInterval = ONE_HOUR_INTVL;
 	triggerRecordPtr->berec.barScale = BAR_SCALE_FULL;
