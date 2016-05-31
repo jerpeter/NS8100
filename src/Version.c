648a50b5aa599ab2924d4b9b0d11cd1576fc3bde
jerpeter <jerpeter@yahoo.com>
2016-05-31 00:38:07 -0500

    Release 2.21.C

--- src/Version.c
+++ src/Version.c
@@ -8,7 +8,7 @@
 //===============================================
 // Build version, system date and time referenced
 //===============================================
-const char g_buildVersion[] = "2.21.B";
+const char g_buildVersion[] = "2.21.C";
 const char g_buildDate[] = __DATE__ " " __TIME__;
 //===============================================
 
