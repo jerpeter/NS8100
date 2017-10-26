c8c07df9ef325a624139ece89a5a9d7ff26d0989
jerpeter <jerpeter@yahoo.com>
2017-10-25 21:04:20 -0500

    Release 2.30.E

--- src/Version.c
+++ src/Version.c
@@ -8,7 +8,7 @@
 //===============================================
 // Build version, system date and time referenced
 //===============================================
-const char g_buildVersion[] = "2.30.D";
+const char g_buildVersion[] = "2.30.E";
 const char g_buildDate[] = __DATE__ " " __TIME__;
 //===============================================
 
