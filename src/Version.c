355c5fc5fb44330b6b2aee0732332f9c7397e9e3
jerpeter <jerpeter@yahoo.com>
2020-01-07 20:54:42 -0600

    Release 2.32.A

--- src/Version.c
+++ src/Version.c
@@ -8,7 +8,7 @@
 //===============================================
 // Build version, system date and time referenced
 //===============================================
-const char g_buildVersion[] = "2.31.B";
+const char g_buildVersion[] = "2.32.A";
 const char g_buildDate[] = __DATE__ " " __TIME__;
 //===============================================
 
